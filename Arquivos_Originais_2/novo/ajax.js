52b1c40fee078c5345e6902f45daae484dc0686d
Allan de Miranda Silva <allandemiranda@gmail.com>
2017-11-09 10:57:36 -0300

    Initial commit

---
+++ Arquivos_Originais_2/novo/ajax.js
@@ -0,0 +1,18 @@
+setInterval(function tabela() {
+    $('#tabela').empty(); //Limpando a tabela
+    $.ajax({
+        type: 'post', //Definimos o método HTTP usado
+        dataType: 'json', //Definimos o tipo de retorno
+        url: 'getDados.php', //Definindo o arquivo onde serão buscados os dados
+        success: function (dados) {
+            for (var i = 0; dados.length > i; i++) {
+                //Adicionando registros retornados na tabela
+                $('#tabela').append('<tr><td>' + dados[i].id + '</td><td>' + dados[i].nome + '</td><td>' + dados[i].email + '</td></tr>');
+            }
+        }
+    });
+},10000);
+
+//$(document).ready(tabela());
+//setInterval(tabela(),1000);
+//setInterval(function(){ alert("Hello"); }, 3000);
